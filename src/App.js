4dd98d4967450e3f99a90946126a1445be8ea57e
Pujari-Rahul <135742143+Pujari-Rahul@users.noreply.github.com>
2024-01-20 22:38:25 +0530

    add all files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Navbar from './components/Navbar';
+import Form from './components/Form';
+import { useState } from 'react';
+import React from 'react';
+import About from './components/About';
+import Alert from './components/Alert';
+import {
+  BrowserRouter as Router,Route,Routes
+} from "react-router-dom";
 function App() {
+  const[mode,setMode]=useState('light');
+  const[alert,setAlert]=useState(null);
+  const showAlert=(message,type)=>{
+    setAlert({
+      msg:message,
+      type:type
+    })
+    setTimeout(()=>{
+      setAlert(null)
+    },1200);
+  }
+  const Togglemode=()=>{
+    if(mode==='light'){
+      setMode('dark');
+      document.body.style.backgroundColor='#042743';
+      showAlert("Dark Mode Has Been Enabled","success")
+    }else{
+      setMode('light');
+      document.body.style.backgroundColor='white';
+      showAlert("Light Mode Has Been Enabled","success")
+    }
+    
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+  <Router>
+  <Navbar title="Textutiles" mode={mode} Togglemode={Togglemode} />
+  <Alert alert={alert}/>
+  <div className='container my-2'>
+   
+  <Routes>
+          <Route path="/about" element={<About/>} mode={mode}> 
+          </Route>
+            <Route path="/" element={
+            <Form heading=" Try TextUtiles - word counter,character counter" showAlert={showAlert} mode={mode}/>}>
+          
+             </Route> 
+  </Routes>
+     </div>
+   </Router> 
+
+
+  </>
   );
 }
 
